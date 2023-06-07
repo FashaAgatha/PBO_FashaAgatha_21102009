cd3a788b808d4669fe52be1c127ab798d1cb7f3d
ASUS <Fashaagatha12@gmail.com>
2023-06-07 14:09:02 +0700

    pertemuan 6

--- src/com/Fasha/PBO/Pertemuan7/Registration.java
+++ src/com/Fasha/PBO/Pertemuan7/Registration.java
@@ -2,14 +2,28 @@ package com.Fasha.PBO.Pertemuan7;
 
 import javax.swing.*;
 
-public class Registration {
-    private JTextField textField1;
-    private JTextField textField2;
+public class Registration extends JFrame {
+    private JTextField textNama;
+    private JTextField textNim;
     private JComboBox comboBox1;
-    private JTextField textField3;
+    private JTextField textTelepon;
     private JRadioButton lakiLakiRadioButton;
     private JRadioButton perempuanRadioButton;
-    private JTextArea textArea1;
+    private JTextArea textAlamat;
     private JButton saveButton;
     private JButton clearButton;
+    private JPanel mainPanel;
+
+    public Registration(){
+        super("Formulir registrasi");
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.setSize(800, 600);
+    }
+
+    public static void main(String[] args){
+        JFrame mainFrame = new Registration();
+        mainFrame.setVisible(true);
+    }
 }
